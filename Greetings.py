c3984573f96e874e67bec7a9083183c5c636fe95
Francisco <147527891+Francisco531970@users.noreply.github.com>
2023-10-10 11:53:00 -0500

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,11 @@
+# Francisco Sanchez
+# 10/3/2023
+
+# This program asks for your first and last name to greet you
+
+# In this part the program asks you to enter your first and last name
+firstName = input("Please enter your first name: ")
+lastName = input("Please enter your last name: ")
+
+# Here the program takes the users input and prints it with the greeting
+print("Hello " + firstName + " " + lastName + ". Nice to meet you!")
