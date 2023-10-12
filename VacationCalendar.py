1ff8701723284a03bd74e4e03db7dce7cdd9f1e2
Francisco <147527891+Francisco531970@users.noreply.github.com>
2023-10-12 13:54:53 -0500

    Add files via upload

---
+++ VacationCalendar.py
@@ -0,0 +1,17 @@
+# Francisco Sanchez
+# 10/12/23
+
+# Prompt the user to enter the starting day number (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+starting_day = int(input("Enter the starting day number (0-6, where 0 is Sunday, 1 is Monday, etc.): "))
+
+# Prompt the user to enter the length of their stay in nights
+length_of_stay = int(input("Enter the length of your stay in nights: "))
+
+# Calculate the day of the week you will return on
+return_day = (starting_day + length_of_stay) % 7
+
+# Define a list of day names
+day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+# Print the result
+print(f"You will return on a {day_names[return_day]}.")
