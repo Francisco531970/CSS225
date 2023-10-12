3e1137de1da8e0ef960696b17de540beb72ad829
Francisco <147527891+Francisco531970@users.noreply.github.com>
2023-10-12 14:00:22 -0500

    Update GreetingUserAndInstructor.py

--- GreetingUserAndInstructor.py
+++ GreetingUserAndInstructor.py
@@ -1,10 +1,15 @@
 # Francisco Sanchez
 # 10/10/2023
 
+# In this program greets the people whose names are in the program
+
+# Here is the list of people allowed in
 allowed_users = ["Francisco", "Robyn"]
 
+# The user enters their name
 user_name = input("Enter your name: ")
 
+# If the correct name is used, it greets them with their name. If the name isn't right, it just greets them.
 if user_name.lower() in allowed_users:
     print(f"Hello, {user_name}!")
 else:
