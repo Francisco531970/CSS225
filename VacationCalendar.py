46ba82af2110ca9f8183c69a5c4813cc45444c80
Francisco <147527891+Francisco531970@users.noreply.github.com>
2023-10-12 13:56:22 -0500

    Update VacationCalendar.py

--- VacationCalendar.py
+++ VacationCalendar.py
@@ -1,10 +1,12 @@
 # Francisco Sanchez
 # 10/12/23
 
-# Prompt the user to enter the starting day number (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
+# This program is going the calculate the day you return from vacation
+
+# The user will enter the starting day number (0 for Sunday, 1 for Monday, ..., 6 for Saturday)
 starting_day = int(input("Enter the starting day number (0-6, where 0 is Sunday, 1 is Monday, etc.): "))
 
-# Prompt the user to enter the length of their stay in nights
+# The user will enter the length of their stay in nights
 length_of_stay = int(input("Enter the length of your stay in nights: "))
 
 # Calculate the day of the week you will return on
