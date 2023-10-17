368549b837f1f4434a4f95cda80269769721575d
Francisco <147527891+Francisco531970@users.noreply.github.com>
2023-10-17 13:42:42 -0500

    Add files via upload

---
+++ TeamAssignment.py
@@ -0,0 +1,25 @@
+# Francisco Sanchez
+# 10/17/23
+
+print("Choose an operation: ")
+print("1. Learn Python")
+print("2. Learn Java")
+print("3. Go swimming")
+print("4. Have dinner")
+print("5. Go to bed")
+print("0. Exit")
+
+choice = input("Enter 1 to 0 to choose the operation: ")
+
+if choice == "1":
+    print("How to learn Python")
+if choice == "2":
+    print("How to learn Java")
+if choice == "3":
+    print("Pool time")
+if choice == "4":
+    print("Eating food")
+if choice == "5":
+    print("Going to sleep")
+if choice == "0":
+    print("Logged out")
